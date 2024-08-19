45f4655cac771f24cc3202532125809ce148c88b
Pontus <pontushenstam.work@gmail.com>
2024-08-19 16:43:03 +0200

    WIP Cleanup

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-function countHypotenuse(){
+function countHypotenuse() {
+  const lengthValue = document.getElementById("length-value").value;
+  const heigthValue = document.getElementById("heigth-value").value;
 
-    const lengthValue = document.getElementById('LengthValue').value
-    const heigthValue = document.getElementById('HeightValue').value
+  const hypotenuse = Math.sqrt(
+    lengthValue * lengthValue + heigthValue * heigthValue
+  );
+  const degree = (Math.atan(heigthValue / lengthValue) * 180) / Math.PI;
 
-    const hypotenuse = Math.sqrt(lengthValue*lengthValue + heigthValue*heigthValue);
+  console.log("degree " + degree);
 
-    // const degree = (Math.asin(heigthValue/lengthValue)*180)/Math.PI
-    const degree = (Math.atan(heigthValue / lengthValue) * 180) / Math.PI;
-
-    console.log("degree " + degree)
-
-    
-    document.getElementById('pyth_result').innerText = roundUpToThreeDecimals(hypotenuse);
+  document.getElementById("pyth-result").innerText =
+    roundUpToThreeDecimals(hypotenuse);
 }
 
+function countUsingTan(event) {
+  if (event.target.id == "tan-heigth-value") {
+    const heigthValue = event.target.value;
+    console.log(event.target.value, " sin");
+    const degreeValue = document.getElementById("degree-value").value;
+    const lengthValue = heigthValue / Math.tan((degreeValue * Math.PI) / 180);
+    console.log(lengthValue, " sin");
+    document.getElementById("tan-length-value").value =
+      roundUpToThreeDecimals(lengthValue);
+  }
+  if (event.target.id == "tan-length-value") {
+    const lengthValue = event.target.value;
+    console.log(event.target.value, " cos");
+    const degreeValue = document.getElementById("degree-value").value;
+    const heigthValue = lengthValue * Math.tan((degreeValue * Math.PI) / 180);
+    console.log(heigthValue, " cos");
+    document.getElementById("tan-heigth-value").value =
+      roundUpToThreeDecimals(heigthValue);
+  }
 
-function countCosSin(event){
-
-    if(event.target.id == 'sinHeightValue'){
-        const heigthValue = event.target.value
-        console.log(event.target.value, " sin")
-        const degreeValue = document.getElementById('degreeValue').value
-        const lengthValue = heigthValue / Math.tan(degreeValue * Math.PI / 180);
-        // const lengthValue = heightValue/Math.sin(degreeValue * Math.PI / 180.0)
-        console.log(lengthValue, " sin")
-        document.getElementById('cosLengthValue').value = roundUpToThreeDecimals(lengthValue);
-    }
-    if(event.target.id == 'cosLengthValue'){
-        const lengthValue = event.target.value
-        console.log(event.target.value, " cos")
-        const degreeValue = document.getElementById('degreeValue').value
-        // const heightValue = lengthValue*Math.sin(degreeValue * Math.PI / 180.0)
-        const heigthValue = lengthValue * Math.tan(degreeValue * Math.PI / 180);
-        console.log(heigthValue, " cos")
-        document.getElementById('sinHeightValue').value = roundUpToThreeDecimals(heigthValue);
-    }
-
-    if(event.target.id == 'degreeValue'){
-        const degreeValue = event.target.value
-        const lengthValue = document.getElementById('cosLengthValue').value
-        // const heightValue = lengthValue*Math.sin(degreeValue * Math.PI / 180.0)
-        const heigthValue = lengthValue * Math.tan(degreeValue * Math.PI / 180);
-        document.getElementById('sinHeightValue').value = roundUpToThreeDecimals(heigthValue);
-    }    
+  if (event.target.id == "degree-value") {
+    const degreeValue = event.target.value;
+    const lengthValue = document.getElementById("tan-length-value").value;
+    const heigthValue = lengthValue * Math.tan((degreeValue * Math.PI) / 180);
+    document.getElementById("tan-heigth-value").value =
+      roundUpToThreeDecimals(heigthValue);
+  }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("heigth-value")
+    .addEventListener("input", countHypotenuse);
+  document
+    .getElementById("length-value")
+    .addEventListener("input", countHypotenuse);
+  countHypotenuse();
 
-document.addEventListener('DOMContentLoaded', function() {
-
-    document.getElementById('HeightValue').addEventListener('input', countHypotenuse)
-    document.getElementById('LengthValue').addEventListener('input', countHypotenuse)
-    countHypotenuse();
-
-    document.getElementById('sinHeightValue').addEventListener('input', countCosSin)
-    document.getElementById('cosLengthValue').addEventListener('input', countCosSin)
-    document.getElementById('degreeValue').addEventListener('input', countCosSin)
-    countCosSin();
+  document
+    .getElementById("tan-heigth-value")
+    .addEventListener("input", countUsingTan);
+  document
+    .getElementById("tan-length-value")
+    .addEventListener("input", countUsingTan);
+  document
+    .getElementById("degree-value")
+    .addEventListener("input", countUsingTan);
+  countUsingTan();
 });
 
-
-
 function roundUpToThreeDecimals(num) {
-    return Math.ceil(num * 1000) / 1000;
+  return Math.ceil(num * 1000) / 1000;
 }
